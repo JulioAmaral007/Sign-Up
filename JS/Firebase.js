f96e02ebc14f4649f7f2eb49c9f97ecec7892cf4
Júlio Cézar <captaingamergroot@gmail.com>
2022-07-27 19:17:46 -0300

    Update!!

--- JS/Firebase.js
+++ JS/Firebase.js
@@ -1,108 +1,78 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-analytics.js";
-  import { getAuth, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
-  import { GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider  } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-analytics.js";
+import { getAuth, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
+import { GoogleAuthProvider, createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js"; 
 
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyCqC9-qaXhnwJB78VTymDor_40J_FLsfxs",
+  authDomain: "login-c9fae.firebaseapp.com",
+  projectId: "login-c9fae",
+  storageBucket: "login-c9fae.appspot.com",
+  messagingSenderId: "750900930912",
+  appId: "1:750900930912:web:83266910712afa95539605",
+  measurementId: "G-TESDJGV7ZK"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCqC9-qaXhnwJB78VTymDor_40J_FLsfxs",
-    authDomain: "login-c9fae.firebaseapp.com",
-    projectId: "login-c9fae",
-    storageBucket: "login-c9fae.appspot.com",
-    messagingSenderId: "750900930912",
-    appId: "1:750900930912:web:83266910712afa95539605",
-    measurementId: "G-TESDJGV7ZK"
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const goo = new GoogleAuthProvider(app);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const auth = getAuth(app);
-  const goo = new GoogleAuthProvider(app);
-  const git = new GithubAuthProvider(app);
-  const face = new FacebookAuthProvider(app);
+google.addEventListener('click', (e) => {
 
-  google.addEventListener('click', (e) => {
+  signInWithPopup(auth, goo)
+.then((result) => {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  const credential = GoogleAuthProvider.credentialFromResult(result);
+  const token = credential.accessToken;
+  // The signed-in user info.
+  const user = result.user;
 
-    signInWithPopup(auth, goo)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
+  window.location.href='home.html';
 
+}).catch((error) => {
+  // Handle Errors here.
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  // The email of the user's account used.
+  const email = error.email;
+  // The AuthCredential type that was used.
+  const credential = GoogleAuthProvider.credentialFromError(error);
+  // ...
+});
+
+})
+
+sign.addEventListener('click', (e) =>{
+
+  e.preventDefault()
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if(name.trim() === '' || email.trim() === '' || password.trim() === ''){
+    alert('Preencha todos os campos')
+    return
+  }
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
     window.location.href='home.html';
 
-  }).catch((error) => {
-    // Handle Errors here.
+  })
+  .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
+    // ..
   });
 
-  })
+})
 
-  facebook.addEventListener('click', (e) => {
-
-    signInWithPopup(auth, face)
-    .then((result) => {
-      // The signed-in user info.
-      const user = result.user;
-  
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      const credential = FacebookAuthProvider.credentialFromResult(result);
-      const accessToken = credential.accessToken;
-  
-      window.location.href='Pages/home.html';
-  
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = FacebookAuthProvider.credentialFromError(error);
-  
-      // ...
-    });
-
-  })
-
-  github.addEventListener('click', (e) => {
-
-    signInWithPopup(auth, git)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-
-    window.location.href='Pages/home.html';
-
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-
-  })
 
 
